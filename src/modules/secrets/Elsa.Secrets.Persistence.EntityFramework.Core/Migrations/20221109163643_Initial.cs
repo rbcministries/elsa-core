86b596bff7023748e6b940afa297faa4dc6d5cb8
BenSeymourODB <ben.seymour@odb.org>
2022-11-09 11:50:04 -0500

    RM netcoreapp3.1 target to generate migration

---
+++ src/modules/secrets/Elsa.Secrets.Persistence.EntityFramework.Core/Migrations/20221109163643_Initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elsa.Secrets.Persistence.EntityFramework.Core.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Elsa");
+
+            migrationBuilder.CreateTable(
+                name: "Secrets",
+                schema: "Elsa",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secrets", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Secrets",
+                schema: "Elsa");
+        }
+    }
+}
